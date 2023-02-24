27ed069ce57da4e49663462dd5fe76f659f36d9d
Dalton <dalton.m.towe@gmail.com>
2023-02-24 16:23:08 -0500

    Made code and tested

--- src/Counts.java
+++ src/Counts.java
@@ -3,7 +3,30 @@ public class Counts
     public static void main(String[] args)
     {
 
-        
+        for (int num = 0; num <= 30; num++)
+        {
+
+            System.out.println("num= " + num);
+
+        }
+
+
+        for (int num = 30; num >= 0; num--)
+        {
+
+            System.out.println("num= " + num);
+
+        }
+
+        for (int num = 0; num <= 18; num +=3)
+        {
+            System.out.println("num = " + num);
+        }
+
+        for (int num = 10; num >= 0; num -=2)
+        {
+            System.out.println("num = " + num);
+        }
 
     }
 }
