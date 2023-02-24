a17e870662e4e282b617ff2dd2e38d1fee0a8a74
Dalton <dalton.m.towe@gmail.com>
2023-02-24 16:31:07 -0500

    added the comments on what each loop is doing

--- src/Counts.java
+++ src/Counts.java
@@ -3,6 +3,7 @@ public class Counts
     public static void main(String[] args)
     {
 
+        //For loop where number starts at 0 till 30 and adds one each time
         for (int num = 0; num <= 30; num++)
         {
 
@@ -10,7 +11,7 @@ public class Counts
 
         }
 
-
+        //for loop where number starts at 30 and subtracts 1 each time till it is at 0
         for (int num = 30; num >= 0; num--)
         {
 
@@ -18,11 +19,13 @@ public class Counts
 
         }
 
+        //for loop number starts at 0 till 18 and counts uo by 3
         for (int num = 0; num <= 18; num +=3)
         {
             System.out.println("num = " + num);
         }
 
+        //for loop number starts at 10 down to 0 and subtracts 2 each time
         for (int num = 10; num >= 0; num -=2)
         {
             System.out.println("num = " + num);
